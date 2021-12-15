0d6122cbc9fe3a130f0aeb96cfc3db01bfbaff28
69Degenerate <kaneki.kenki.1999@gmail.com>
2021-12-15 22:39:43 +0530

    assignmnt

--- os/assignment 1/setA1.c
+++ os/assignment 1/setA1.c
@@ -1,2 +1,22 @@
 #include<stdio.h>
-#include<sys/dir.h>
+#include<sys/types.h>
+void child();
+void parent();
+int main()
+{
+pid_t pid;
+pid=fork();
+if(pid==0)
+child();
+else
+parent();
+return 0;
+}
+void child()
+{
+printf("child i am,");
+}
+void parent()
+{
+printf("im dissapoint...........");
+}
---
+++ os/assignment 1/setA2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int pid,retnice;
+printf("press DEL to stop \n");
+pid=fork();
+for(;;)
+{
+if(pid==0)
+{
+retnice=nice(-5);
+printf("child get high %d\n",retnice);
+sleep(0);
+}
+else
+{
+retnice=nice(4);
+printf("parent get high %d\n",retnice);
+sleep(0);
+}
+}
+}
