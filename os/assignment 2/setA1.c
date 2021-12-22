48dad45fbf97cb4253e51dc323b005520cd5d1e3
69Degenerate <kaneki.kenki.1999@gmail.com>
2021-12-22 20:39:11 +0530

    correction

--- os/assignment 2/setA1.c
+++ os/assignment 2/setA1.c
@@ -1,30 +1,30 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<dirent.h>
-#include<fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <fcntl.h>
 
-void count(char c,char *fn)
+void count(char c, char *fn)
 
 {
-    int cc=0 ,lc=0 ,wc=0,handle;
+    int cc = 0, lc = 0, wc = 0, handle;
     char ch;
-    if (handle=open(fn ,O_RDONLY)==-1)
+    if (handle = open(fn, O_RDONLY) == -1)
     {
-        printf("file %s not found",fn);    /* code */
+        printf("file %s not found", fn); /* code */
         return;
     }
-    while (read(handle,&ch,1)!=0)
+    while (read(handle, &ch, 1) != 0)
     {
-        if (ch==' ')
+        if (ch == ' ')
         {
             wc++;
             /* code */
         }
-        if (ch=='\n')
+        if (ch == '\n')
         {
-            lc++;/* code */
+            lc++; /* code */
         }
         else
         {
@@ -32,62 +32,62 @@ void count(char c,char *fn)
         }
         /* code */
     }
-    close (handle);    
+    close(handle);
     switch (c)
     {
     case 'c':
-        printf("total number of character:%d",cc);/* code */
+        printf("total number of character:%d", cc); /* code */
         break;
     case 'w':
-        printf("total no of words:%d",wc);
+        printf("total no of words:%d", wc);
         break;
     case 'i':
-        printf("total number of lines:%D",lc);
+        printf("total number of lines:%D", lc);
         break;
     }
 }
 
 main()
 {
-    char command [80],t1[20],t2[20],t3[20],t4[20];
+    char command[80], t1[20], t2[20], t3[20], t4[20];
     int n;
     system("clear");
     while (1)
     {
         printf("\n MYSHELL$:"); /* code */
         fflush(stdin);
-        fgets(command ,80,stdin);
-        n=sscanf(command,"%s%s%s%s",t1,t2,t3,t4);
+        fgets(command, 80, stdin);
+        n = sscanf(command, "%s%s%s%s", t1, t2, t3, t4);
         switch (n)
         {
-        case 1/* constant-expression */:
+        case 1 /* constant-expression */:
             if (!fork())
             {
-                execlp (t1,t1,NULL);
+                execlp(t1, t1, NULL);
                 perror(t1);
-                 /* code */
+                /* code */
             }
             /* code */
             break;
         case 2:
             if (!fork())
             {
-                execlp (t1,t1,t2,NULL);
+                execlp(t1, t1, t2, NULL);
                 perror(t1);
-                 /* code */
+                /* code */
             }
             break;
         case 3:
-            if (strcmp(t1,"count")==0)
+            if (strcmp(t1, "count") == 0)
             {
-                count(t2[0],t3);
-                 /* code */
+                count(t2[0], t3);
+                /* code */
             }
             else
             {
                 if (!fork())
                 {
-                    execlp (t1,t1,t2,t3,NULL);
+                    execlp(t1, t1, t2, t3, NULL);
                     perror(t1);
                     /* code */
                 }
@@ -96,12 +96,11 @@ main()
         case 4:
             if (!fork())
             {
-                execlp (t1,t1,t2,t3,t4,NULL);
+                execlp(t1, t1, t2, t3, t4, NULL);
                 perror(t1);
-                 /* code */
+                /* code */
             }
             break;
         }
     }
-    
 }
--- os/assignment 2/setB1.c
+++ os/assignment 2/setB1.c
@@ -1,46 +1,46 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<unistd.h>
-#include<dirent.h>
-#include<fcntl.h>
-void list(char c,char *dn)
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <fcntl.h>
+void list(char c, char *dn)
 {
     DIR *dir;
-    int i,cnt=0;
+    int i, cnt = 0;
     struct dirent *entry;
     struct stat buff;
-    if((dir=opendir(dn))==NULL)
+    if ((dir = opendir(dn)) == NULL)
     {
-        printf("File not found:",dn);
+        printf("File not found:", dn);
         return;
     }
-    switch(c)
+    switch (c)
     {
-        case 'f':
-        while((entry=readdir(dir))!=NULL)
+    case 'f':
+        while ((entry = readdir(dir)) != NULL)
         {
-            stat(entry->d_name,&buff);
-            if(S_IFREG&buff.st_mode)
-           {
-                printf("%s\n",entry->d_name);
-           }
+            stat(entry->d_name, &buff);
+            if (S_IFREG & buff.st_mode)
+            {
+                printf("%s\n", entry->d_name);
+            }
         }
         break;
-        case 'n':
-        while((entry=readdir(dir))!=NULL)
+    case 'n':
+        while ((entry = readdir(dir)) != NULL)
         {
             cnt++;
-            printf("The no of entries =%d",cnt);
+            printf("The no of entries =%d", cnt);
         }
         break;
-        case 'i':
-        while((entry=readdir(dir))!=NULL)
+    case 'i':
+        while ((entry = readdir(dir)) != NULL)
         {
-            stat(entry->d_name,&buff);
-            if(S_IFREG&buff.st_mode)
+            stat(entry->d_name, &buff);
+            if (S_IFREG & buff.st_mode)
             {
-                printf("%s\t %d \n",entry->d_name,&buff.st_ino);
+                printf("%s\t %d \n", entry->d_name, &buff.st_ino);
             }
         }
         break;
@@ -49,49 +49,49 @@ void list(char c,char *dn)
 }
 main()
 {
-    char command[80],t1[20],t2[20],t3[20],t4[20];
+    char command[80], t1[20], t2[20], t3[20], t4[20];
     int n;
     system("clear");
-    while(1)
+    while (1)
     {
         printf("myshell$");
         fflush(stdin);
-        fgets(command,80,stdin);
-        n=sscanf(command,"%s %s %s %s",t1,t2,t3,t4);
-        switch(n)
+        fgets(command, 80, stdin);
+        n = sscanf(command, "%s %s %s %s", t1, t2, t3, t4);
+        switch (n)
         {
-            case 1:
-            if(!fork())
+        case 1:
+            if (!fork())
             {
-                execlp(t1,t1,NULL);
+                execlp(t1, t1, NULL);
                 perror(t1);
             }
             break;
-            case 2:
-            if(!fork())
+        case 2:
+            if (!fork())
             {
-                execlp(t1,t1,t2,NULL);
+                execlp(t1, t1, t2, NULL);
                 perror(t1);
             }
             break;
-            case 3:
-            if(strcmp(t1,"list")==0)
+        case 3:
+            if (strcmp(t1, "list") == 0)
             {
-                list(t2[0],t3);
+                list(t2[0], t3);
             }
             else
             {
-                if(!fork())
+                if (!fork())
                 {
-                    execlp(t1,t1,t2,t3,NULL);
+                    execlp(t1, t1, t2, t3, NULL);
                     perror(t1);
                 }
             }
             break;
-            case 4:
-            if(!fork())
+        case 4:
+            if (!fork())
             {
-                execlp(t1,t1,t2,t3,t4);
+                execlp(t1, t1, t2, t3, t4);
                 perror(t1);
             }
             break;
