ae864b09bf3357f512d55bed08a0ed64373fa222
69Degenerate <kaneki.kenki.1999@gmail.com>
2022-02-19 15:12:18 +0530

    1st

---
+++ practical_slips/java/slip1/1st.java
@@ -0,0 +1,13 @@
+public class Characters {
+    public static void main(String[] args) {
+    char c;
+    for(c = 'A'; c <= 'Z'; ++c)
+    // for uppercase
+    
+    // for(c = 'a'; c <= 'z'; ++c)
+    // for lowercase
+
+
+    System.out.print(c + " ");
+    }
+    }
